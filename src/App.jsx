f34e1e1af2f9e9e6e37271b6b6f69cb93f402ca4
Emirhan Çam <114196329+zzistaken@users.noreply.github.com>
2024-04-29 11:17:32 +0300

    Update
    useEffect, Axios and JSON Server have been added. In this way, data loss will not occur even if the project is refreshed on the browser.

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,43 @@ import "./App.css";
 import Hero from "./components/Hero";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const createTask = (title, detail) => {
-    const currentTasks = [
-      ...tasks,
-      {
-        id: Math.round(Math.random() * 999999),
-        title,
-        detail,
-      },
-    ];
+  const getTasks = async () => {
+    const response = await axios.get("http://localhost:3005/tasks");
+    setTasks(response.data);
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const createTask = async (title, detail) => {
+    const response = await axios.post("http://localhost:3005/tasks", {
+      title,
+      detail,
+    });
+    const currentTasks = [...tasks, response.data];
     setTasks(currentTasks);
   };
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await axios.delete(`http://localhost:3005/tasks/${id}`);
     const remainingTasks = tasks.filter((task) => {
       return task.id !== id;
     });
     setTasks(remainingTasks);
   };
 
-  const updateTask = (id, updatedTitle, updatedDetail) => {
+  const updateTask = async (id, updatedTitle, updatedDetail) => {
+    await axios.put(`http://localhost:3005/tasks/${id}`, {
+      title: updatedTitle,
+      detail: updatedDetail,
+    });
     const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         return {
